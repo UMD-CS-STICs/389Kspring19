b94cbc060a28fa5068a0cf77bbd0353224fbccb5
Benny <bcheng96@umd.edu>
2019-02-07 21:23:49 -0500

    added in class code demo

---
+++ lectures/week_02-javascript/code/javascript.js
@@ -0,0 +1,155 @@
+// ARRAYS
+var names = ['Benny', 'Chirag', 'Dickerson']
+var stuff = [1, 2+4, "Mynamejeff", function(){return 1}]
+
+// console.log(stuff)
+
+
+
+
+
+
+
+// FOR LOOPS
+for (var i = 0; i < names.length; i++) {
+   // console.log(names[i]);
+}
+
+for (var i in names) {
+     // console.log(i);
+}
+
+for (var i in names) {
+    // console.log(names[i]);
+}
+
+//Introduced in ES2015
+for (var i of names) {
+	// console.log(i);
+}
+
+
+
+
+
+
+
+// FUNCTIONS
+
+// notice how we do not need to specify type (:
+function greet(name) {
+    console.log('Hello ' + name);
+}
+
+// greet('Benny');
+
+function reverser(name) {
+    var arr = name.split(''); // arr is now an array of strings
+    arr = arr.reverse();
+    arr = arr.join(''); // note: the type of arr is now just a single string!
+    console.log(arr);
+}
+
+function printFirstLetter(name) {
+    console.log(name.charAt(0));
+}
+
+// reverser('Benny'); 
+// printFirstLetter('Benny'); 
+// printFirstLetter(); 
+
+// reverser(123);
+
+
+
+
+
+
+// FIRST CLASS FUNCTIONS
+
+function applyToArray(operator) {
+    console.log(operator);
+}
+
+// applyToArray(reverser);
+
+function hi() {
+    console.log('hi');
+}
+
+function bye() {
+    console.log('bye');
+}
+
+function benny() {
+    console.log('benny');
+}
+
+var funcs = [hi, bye, benny]
+// Exercise: Iterate through this array and exucte each function
+
+
+
+
+
+
+// ANONYMOUS FUNCTIONS - function definition not bound to an identifier
+var funcs = [
+    function() {
+        console.log('hi');
+    },
+    function() {
+        console.log('bye');
+    },
+    function() {
+        console.log('benny');
+    }
+];
+
+
+
+// FIRST CLASS FUNCTIONS 2
+
+// Exercise: Modify applyToArray(operator) to print ynneB
+function applyToArray(operator) {
+
+}
+
+// applyToArray(reverser); 
+
+
+
+
+
+
+function applyToArray(arr, operator) {
+    var name = arr[0];
+    operator(name);
+}
+
+// var names = ['Benny', 'Chirag', 'Dickerson'];
+// applyToArray(names, reverser); 
+// applyToArray(names, greet);
+
+
+
+// Run operator across all elemnts of arr
+function applyToArray(arr, operator) {
+    // ??
+}
+
+
+
+
+// FIRST CLASS + ANONYMOUS
+var num = [1, 2, 3, 4, 5];
+applyToArray(num, function(i) {
+    console.log(i * i);
+});
+
+
+
+
+
+
+
---
+++ lectures/week_02-javascript/code/object.js
@@ -0,0 +1,73 @@
+// "constructor"
+var people = {};
+
+// initialize our people "class"
+people.Benny = 20;
+people.Chirag = 21;
+people.Dickerson = 90;
+
+// The following also works.
+people['Benny'] = 20;
+people['Chirag'] = 21;
+people['Dickerson'] = 90;
+
+console.log(people);
+
+
+// Alternatively
+
+// key/value pair
+var people = {
+    Benny: 20,
+    Chirag: 21,
+    Dickerson: 90
+};
+
+
+// nesting
+var people = {
+    Benny: {
+        age: 20,
+        gender: 'male'
+    },
+    Chirag: {
+        age: 21,
+        gender: 'male'
+    },
+    Dickerson: {
+        age: 90,
+        gender: 'male'
+    }
+};
+
+// console.log(people['Dickerson']);
+// console.log(people.Dickerson); 
+
+
+// Iterate through keys
+for (var name in people) {
+//     console.log(name);
+}
+
+// Let's print out the name and ages
+for (var name in people) {
+ 	// Note this string interpolation / string literal feature - introduced in ES2015!
+//     console.log(`${name}'s age is ${people[name].age}.`);
+}
+
+// Question: Why would doing people.name.age not work?
+
+
+
+
+
+people.Dickerson.getAgeSquared = function () {
+    return people.Dickerson.age * people.Dickerson.age;
+};
+
+// console.log(people.Dickerson.getAgeSquared());
+
+people.Dickerson.age = 12;
+//console.log(people.Dickerson.getAgeSquared());
+
+// Question: What would happen if I ran 'people.Dickerson.getAgeSquared = 2' followed by 'people.Dickerson.getAgeSquared()'?

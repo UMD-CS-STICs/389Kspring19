8b148e9515c5872596a9bb1d5b3a1146e51aab86
Benny Cheng <31495579+bcheng1996@users.noreply.github.com>
2019-02-20 21:51:45 -0500

    Update app.js

--- lectures/week_04-nodejs/code/app.js
+++ lectures/week_04-nodejs/code/app.js
@@ -3,23 +3,7 @@ const PORT = 8888;
 
 function handleReq(req, res) {
     console.log("New request at " + req.url);
-    if (req.url === '/nazifa') {
-        var nazifa = {
-            age: 21,
-            gender: 'f',
-            majors: ['computer science']
-        };
-        res.end(nazifa);
-    } else if (req.url === '/tim') {
-        var tim = {
-            age: 21,
-            gender: 'm',
-            majors: ['computer science', 'finance']
-        };
-        res.end(tim);
-    } else {
-        res.end("Link hit: " + req.url);
-    }
+    
 }
 
 var server = http.createServer(handleReq);
